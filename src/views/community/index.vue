757c86086079a13b5f195f3348af3da52b983bf0
KAKAOVX <>
2022-06-21 10:56:09 +0900

    폴더구조 잡는중

--- src/views/community/index.vue
+++ src/views/community/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="join">
-    <h1>This is an join page</h1>
+  <div class="community">
+    <h1>This is an about page</h1>
   </div>
 </template>
 
@@ -8,6 +8,6 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'JoinPage',
+  name: 'CommunityPage',
 })
 </script>
