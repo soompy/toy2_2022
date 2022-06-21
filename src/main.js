bd3567bccfc8ed6d6d7591a05e1f376d5ea1efc2
KAKAOVX <>
2022-06-21 11:52:19 +0900

    폴더구조 최종

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import "./index.css";
 import router from './router';
-import store from './store';
+// import store from './store';
 
 createApp(App).use(router).mount('#app')
-const app = createApp(App).use(router).use(store);
+// const app = createApp(App).use(router).use(store);
